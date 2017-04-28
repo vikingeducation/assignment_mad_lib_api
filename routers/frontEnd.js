5bb60b7f8b5caea73f7e86cbfe1c6f56511a1423
Egle Libby <eglelibby@gmail.com>
2017-04-28 18:58:37 -0400

    finish routes for front end

--- routers/frontEnd.js
+++ routers/frontEnd.js
@@ -9,7 +9,7 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
   request.post(
     {
-      url: `http://localhost:3000/api/v1/mad_lib?token=a3c6805d57a0bd10b3e7e12caa249025`,
+      url: `http://localhost:3000/api/v1/mad_lib?token=42e472294cbcb5a4033983859b242c0c`,
       form: {
         words: req.body.words.split(" "),
         story: req.body.story
@@ -17,7 +17,51 @@ router.post("/", (req, res) => {
     },
     (err, response, body) => {
       let story = JSON.parse(body);
+      story = story.charAt(0).toUpperCase() + story.slice(1);
+      if (story[story.length - 1] != ".") {
+        story += ".";
+      }
       res.render("frontEnd", { story });
+    }
+  );
+});
+
+router.get("/nouns", (req, res) => {
+  request.get(
+    "http://localhost:3000/api/v1/nouns?token=42e472294cbcb5a4033983859b242c0c",
+    (err, response, body) => {
+      let nouns = JSON.parse(body);
+      res.render("frontEnd", { nouns });
+    }
+  );
+});
+
+router.get("/verbs", (req, res) => {
+  request.get(
+    "http://localhost:3000/api/v1/verbs?token=42e472294cbcb5a4033983859b242c0c",
+    (err, response, body) => {
+      let verbs = JSON.parse(body);
+      res.render("frontEnd", { verbs });
+    }
+  );
+});
+
+router.get("/adjectives", (req, res) => {
+  request.get(
+    "http://localhost:3000/api/v1/adjectives?token=42e472294cbcb5a4033983859b242c0c",
+    (err, response, body) => {
+      let adjectives = JSON.parse(body);
+      res.render("frontEnd", { adjectives });
+    }
+  );
+});
+
+router.get("/adverbs", (req, res) => {
+  request.get(
+    "http://localhost:3000/api/v1/adverbs?token=42e472294cbcb5a4033983859b242c0c",
+    (err, response, body) => {
+      let adverbs = JSON.parse(body);
+      res.render("frontEnd", { adverbs });
     }
   );
 });
