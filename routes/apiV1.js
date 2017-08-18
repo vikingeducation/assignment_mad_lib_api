d58a027d7d2dc2ce82b3ed0600c025d11f71acc6
Will Timpson <will.timpson@gmail.com>
2017-08-18 16:23:07 -0400

    to fancy by half refactor

--- routes/apiV1.js
+++ routes/apiV1.js
@@ -4,28 +4,45 @@ const wp = new WordPOS();
 
 router.get("/", (req, res) => res.json({ name: "Mad Lib API" }));
 
-router.get("/nouns", (req, res, next) => {
+const typeMap = {
+  nouns: wp.randNoun,
+  verbs: wp.randVerb,
+  adverbs: wp.randAdverb,
+  adjectives: wp.randAdjective
+};
+
+router.get("/:type", (req, res, next) => {
   const count = req.query.count || 10;
-  wp.randNoun({ count }).then(words => res.json(words)).catch(e => next(e));
+  const typeFunc = typeMap[req.params.type];
+  if (typeFunc) {
+    typeFunc({ count }).then(words => res.json(words)).catch(e => next(e));
+  } else {
+    next();
+  }
 });
 
-router.get("/verbs", (req, res, next) => {
-  const count = req.query.count || 10;
-  wp.randVerb({ count }).then(words => res.json(words)).catch(e => next(e));
-});
+// router.get("/nouns", (req, res, next) => {
+//   const count = req.query.count || 10;
+//   wp.randNoun({ count }).then(words => res.json(words)).catch(e => next(e));
+// });
 
-router.get("/adverbs", (req, res, next) => {
-  const count = req.query.count || 10;
-  wp.randAdverb({ count }).then(words => res.json(words)).catch(e => next(e));
-});
+// router.get("/verbs", (req, res, next) => {
+//   const count = req.query.count || 10;
+//   wp.randVerb({ count }).then(words => res.json(words)).catch(e => next(e));
+// });
 
-router.get("/adjectives", (req, res, next) => {
-  const count = req.query.count || 10;
-  wp
-    .randAdjective({ count })
-    .then(words => res.json(words))
-    .catch(e => next(e));
-});
+// router.get("/adverbs", (req, res, next) => {
+//   const count = req.query.count || 10;
+//   wp.randAdverb({ count }).then(words => res.json(words)).catch(e => next(e));
+// });
+
+// router.get("/adjectives", (req, res, next) => {
+//   const count = req.query.count || 10;
+//   wp
+//     .randAdjective({ count })
+//     .then(words => res.json(words))
+//     .catch(e => next(e));
+// });
 
 router.get("*", (req, res) => {
   res.json({ error: "404 Not Found" });
