a7e47aec5ab2d7524b65613c45eda49b888d6d0c
Eric Glover <etgt28@mail.missouri.edu>
2017-08-18 11:15:51 -0500

    front-end and passport things

--- app.js
+++ app.js
@@ -2,22 +2,78 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const { User } = require("./models");
 
 app.use((req, res, next) => {
-	if (mongoose.connection.readyState) {
-		next();
-	} else {
-		require("./mongo")().then(() => next());
-	}
+  if (mongoose.connection.readyState) {
+    next();
+  } else {
+    require("./mongo")().then(() => next());
+  }
 });
 
+//Passport Goodness
 app.use(passport.initialize());
 app.use(passport.session());
 
+const LocalStrategy = require("passport-local").Strategy;
+const TokenStrategy = require("passport-http-bearer").Strategy;
+
+const localStrategy = new LocalStrategy(
+  {
+    usernameField: email
+  },
+  (email, password, done) => {
+    User.find({
+      email: email
+    })
+      .then(user => {
+        const valid = User.validPassword(password);
+        return done(null, valid ? user : false);
+      })
+      .catch(e => {
+        done(null, false);
+      });
+  }
+);
+
+const tokenStrategy = new TokenStrategy((token, done) => {
+  User.findOne({
+    token: token
+  })
+    .then(user => {
+      return done(null, user || false);
+    })
+    .catch(e => {
+      done(null, false);
+    });
+});
+passport.use(localStrategy);
+passport.use(tokenStrategy);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+    .catch(e => {
+      done(null, false);
+    });
+});
+
+//HANDLEBARS
+const hbs = require("express-handlebars");
+
+app.engine("handlebars", hbs.create({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 app.get("/login", (req, res) => {
-	// render login page
+  // render login page
 });
 
 app.listen(3000, () => {
-	console.log("Now listening...");
+  console.log("Now listening...");
 });
