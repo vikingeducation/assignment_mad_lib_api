3519fa8a1a81fb3c7ea9d829854750903b59f678
Kara Thrash <dvdonato@gmail.com>
2017-08-18 13:26:26 -0400

    madlibs

--- utils/madlib.js
+++ utils/madlib.js
@@ -1,23 +1,40 @@
-const Sentencer = require('sentencer');
-const WordPOS = require('wordpos');
+const Sentencer = require("sentencer");
+const WordPOS = require("wordpos");
 const wordpos = new WordPOS();
 
 // ['barge', 'barf', 'barter_away'] 'bar'
 
-Sentencer.configure({
-	nounList: [],
-	adjectiveList: [],
-	verbList: []
-});
-
 module.exports = {
-	createMadlib: () => {
-		Sentencer.make('hello my name is {{ noun }} and ');
-	}
+  createMadlib: () => {
+    Sentencer.make("hello my name is {{ noun }} and {{}}");
+  }
 };
 
 async function generateWords() {
-	let random = {};
+  let random = {};
 
-	let verbArr = await wordpos.randVerb({ count: 3 });
+  let verbArr = await wordpos.randVerb({ count: 20 });
+  let nounArr = await wordpos.randVerb({ count: 20 });
+  let adjArr = await wordpos.randAdjective({ count: 20 });
+  // verbList = verbArr;
+  // nounList = nounArr;
+  // adjectiveList = adjArr;
+  Sentencer.configure({
+    nounList: nounArr,
+    adjectiveList: adjArr,
+    verbList: verbArr
+  });
+  console.log(
+    Sentencer.make("hello my name is {{ noun }} and I am {{ adjective }}")
+  );
 }
+
+async function ParseSentence(sentence) {
+  // let sentenceArray = sentence.split(" ");
+  // sentenceArray.forEach(i =>{
+  // 	let noun
+  // 	if(wordpos.isNoun(i, callback))
+  // });
+}
+
+generateWords();
