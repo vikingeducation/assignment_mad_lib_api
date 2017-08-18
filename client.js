3d5209c27ce782a5cdd4a00ad2323c60b9182863
Will Timpson <will.timpson@gmail.com>
2017-08-18 19:04:40 -0400

    almost client

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const request = require("request");
+const rp = require("request-promise");
 
 // Post Data
 const bodyParser = require("body-parser");
@@ -21,11 +22,43 @@ const morganToolkit = require("morgan-toolkit")(morgan);
 app.use(morganToolkit());
 
 // Route Handlers
+
 app.get("/", (req, res) => {
   res.render("client/index");
 });
 
-app.post("/", (req, res) => {
+app.post("/prepare", async (req, res) => {
+  const token = req.body.token;
+  try {
+    const promises = [
+      rp.get(`http://localhost:3000/api/v1/nouns?access_token=${token}`),
+      rp.get(`http://localhost:3000/api/v1/verbs?access_token=${token}`),
+      rp.get(`http://localhost:3000/api/v1/adverbs?access_token=${token}`),
+      rp.get(`http://localhost:3000/api/v1/adjectives?access_token=${token}`)
+    ];
+    let [nouns, verbs, adverbs, adjectives] = await Promise.all(promises);
+    [nouns, verbs, adverbs, adjectives] = [
+      nouns,
+      verbs,
+      adverbs,
+      adjectives
+    ].map(type => {
+      JSON.parse(type);
+    });
+    const words = [
+      { name: "Nouns", words: nouns },
+      { name: "Verbs", words: verbs },
+      { name: "Adverbs", words: adverbs },
+      { name: "Adjectives", words: adjectives }
+    ];
+    console.log(words);
+    res.render("client/build", { words, token });
+  } catch (error) {
+    res.end(error.stack);
+  }
+});
+
+app.post("/build", (req, res) => {
   let { token, template, words } = req.body;
   words = words.split(" ");
   const url = `http://localhost:3000/api/v1/sentences?access_token=${token}`;
