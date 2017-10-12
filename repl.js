1e1c4e1a1ffddc7a07a799871b04547f057fc19a
Chuck Michael <chuckqs@yahoo.com>
2017-10-11 22:20:27 -0400

    User creation

---
+++ repl.js
@@ -0,0 +1,34 @@
+const repl = require("repl").start({});
+const faker = require("faker");
+const mongoose = require("mongoose");
+const models = require("./models");
+// const helpers = require('./helpers');
+const md5 = require("md5");
+const uuid = require("uuid");
+
+require("./mongo")().then(() => {
+  // ----------------------------------------
+  // Models/Helpers
+  // ----------------------------------------
+  repl.context.models = models;
+  // repl.context.helpers = helpers;
+
+  // ----------------------------------------
+  // Mongoose
+  // ----------------------------------------
+  Object.keys(models).forEach(key => {
+    repl.context[key] = mongoose.model(key);
+  });
+
+  // ----------------------------------------
+  // Libs
+  // ----------------------------------------
+  repl.context.faker = faker;
+  repl.context.md5 = md5;
+  repl.context.uuid = uuid;
+
+  // ----------------------------------------
+  // Logging
+  // ----------------------------------------
+  repl.context.lg = console.log;
+});
---
+++ routers/login.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const models = require("./../models");
+const User = mongoose.model("User");
+
+module.exports = middlewares => {
+  const { loggedInOnly, loggedOutOnly } = middlewares;
+
+  // ----------------------------------------
+  // Show
+  // ----------------------------------------
+  const onShow = (req, res) => {
+    User.findById(req.session.passport.user).then(user => {
+      res.render("user/view", { user });
+    });
+  };
+
+  // Showing a user only if logged in
+  router.get("/", loggedInOnly, onShow);
+  router.get("/user", loggedInOnly, onShow);
+
+  //–––––––––––––
+  // New
+  //–––––––––––––
+  router.get("/register", loggedOutOnly, (req, res) => {
+    res.render("login/register");
+  });
+
+  //–––––––––––––
+  // Create
+  //–––––––––––––
+  router.post("/register", loggedOutOnly, (req, res) => {
+    const userParams = {
+      fname: req.body.fname,
+      lname: req.body.lname,
+      email: req.body.email,
+      password: req.body.password
+    };
+    User.create(userParams)
+      .then(user => {
+        req.session.user = user;
+        req.login(user, function() {
+          res.redirect("/login");
+        });
+      })
+      .catch(e => {
+        console.log(e);
+        res.redirect("/");
+      });
+  });
+
+  return router;
+};
