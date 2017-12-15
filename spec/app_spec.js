09df9c2f7e754e711bce852d10fab2515696f471
Hai Nguyen <memyselfandhai@gmail.com>
2017-12-15 12:34:06 -0600

    started testing

---
+++ spec/app_spec.js
@@ -0,0 +1,53 @@
+// spec/app_spec.js
+const app = require("../app");
+const User = require("../models/user");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/mad_lib_test");
+const request = require("request");
+
+describe("App", () => {
+  const baseUrl = "http://localhost:3000";
+  const apiUrl = baseUrl + "/api/v1/";
+  let server;
+
+  let user;
+
+  beforeEach(done => {
+    User.create({
+      fname: "Foo",
+      lname: "Bar",
+      email: "foobar@gmail.com",
+      password: "password1234"
+    }).then(result => {
+      user = result;
+      done();
+    });
+  });
+
+  beforeAll(done => {
+    server = app.listen(3000, () => {
+      done();
+    });
+  });
+
+  afterAll(done => {
+    server.close();
+    server = null;
+    done();
+  });
+
+  it("renders the home page", done => {
+    request.get(baseUrl, (err, res, body) => {
+      expect(res.statusCode).toBe(200);
+      expect(body).toMatch(/api/i);
+      done();
+    });
+  });
+
+  it("returns a list of nouns"),
+    done => {
+      request.get(`baseUrl/nouns`, (err, res, body) => {
+        expect(body).isArray(body);
+      });
+    };
+});
---
+++ spec/helpers/spec_helper.js
@@ -0,0 +1,27 @@
+// spec/helpers/spec_helper.js
+
+// Set test environment
+process.env.NODE_ENV = "test";
+
+// spec/helpers/spec_helper.js
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/mad_lib_test");
+
+// Set test environment
+process.env.NODE_ENV = "test";
+
+beforeAll(done => {
+  if (mongoose.connection.readyState) {
+    done();
+  } else {
+    require("./../../mongo")().then(() => done());
+  }
+});
+
+afterEach(done => {
+  require("./../../seeds/clean")()
+    .then(() => done())
+    .catch(e => console.error(e.stack));
+});
