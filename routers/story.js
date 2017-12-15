56efb730bb16850ab9b02df0c866c2809d428745
EdwinYung <eyung7@gmail.com>
2017-12-15 17:02:03 -0500

    working on replace

--- routers/story.js
+++ routers/story.js
@@ -1,16 +1,41 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const helpers = require("./../helpers");
+const helpers = require('./../helpers');
 const h = helpers.registered;
-var WordPOS = require("wordpos"),
+var WordPOS = require('wordpos'),
   wordpos = new WordPOS();
+var Sentencer = require('sentencer');
+
+// Sentencer.configure({
+//   actions: {
+//     number: function() {
+//       return Math.floor( Math.random() * 10 ) + 1;
+//     }
+//   }
+// });
+
 // ----------------------------------------
 // Story
 // ----------------------------------------
-router.post("/", async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   let story = req.body.sentence;
   let words = await wordpos.getPOS(story, console.log);
-  let templated = story.replace();
+  let templated = await function(words) {
+    let temp = story;
+    for (let key in words) {
+      if (key !== 'rest') {
+        words[key].forEach(element => {
+          temp.replace(element, `{{ ${key} }}`);
+        });
+      }
+    }
+    return temp;
+  };
+  let result = templated(words);
+  //let templated = story.replace();
+  console.log('=======================');
+  console.log(result);
+  console.log('=======================');
   res.status(200).json(result);
 });
 
